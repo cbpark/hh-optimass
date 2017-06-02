bf107c58aab63c8f8a20585d4c5b25b896c9e23c
cbpark <cbpark@gmail.com>
2017-06-02 19:38:26 +0900

    Fix hh_optimass_parton.cc

--- src/hh_optimass_parton.cc
+++ src/hh_optimass_parton.cc
@@ -6,7 +6,6 @@
  *  go to <http://www.gnu.org/licenses/> for full license details.
  */
 
-#include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -20,15 +19,14 @@ const char appname[] = "hh_optimass_parton";
 
 int main(int argc, char *argv[]) {
     if (argc != 2) { return howToUse(appname, "<input>"); }
+    const auto to_out = &cout;  // information will be displayed in screen.
 
     std::ifstream fin{argv[1]};
     if (fin.fail()) {
         return failToRead(appname, argv[1]);
     } else {
-        cout << appname << ": reading `" << argv[1] << "' ...\n";
+        message(appname, "reading `" + std::string(argv[1]) + "' ...", to_out);
     }
-
-    const auto to_out = &cout;  // information will be displayed in screen.
 
     lhef::Event lhe = lhef::parseEvent(&fin);
     unsigned long neve = 0;
